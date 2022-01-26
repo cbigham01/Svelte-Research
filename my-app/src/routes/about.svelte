23b6179b81427ea77f097d181349e77a47032833
Cal Bigham <cbigham@monmouthcollege.edu>
2022-01-26 13:20:17 -0600

    first commit

---
+++ my-app/src/routes/about.svelte
@@ -0,0 +1,56 @@
+<script context="module">
+	import { browser, dev } from '$app/env';
+
+	// we don't need any JS on this page, though we'll load
+	// it in dev so that we get hot module replacement...
+	export const hydrate = dev;
+
+	// ...but if the client-side router is already loaded
+	// (i.e. we came here from elsewhere in the app), use it
+	export const router = browser;
+
+	// since there's no dynamic data here, we can prerender
+	// it so that it gets served as a static asset in prod
+	export const prerender = true;
+</script>
+
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+<div class="content">
+	<h1>About this app</h1>
+
+	<p>
+		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
+		following into your command line and following the prompts:
+	</p>
+
+	<!-- TODO lose the @next! -->
+	<pre>npm init svelte@next</pre>
+
+	<p>
+		The page you're looking at is purely static HTML, with no client-side interactivity needed.
+		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
+		the devtools network panel and reloading.
+	</p>
+
+	<p>
+		<strong> This part of the app is simple. I did not change anything, as there is nothing to learn here. It is pure HTML, through and through.
+		I could add some JavaScript type elements to this, but I will do that on what was originally the TODO page.
+		I am going to change the name of this page to My Toy.</strong>
+	</p>
+
+	<p>
+		The <a href="/todos">TODOs</a> page illustrates SvelteKit's data loading and form handling. Try using
+		it with JavaScript disabled!
+	</p>
+</div>
+
+<style>
+	.content {
+		width: 100%;
+		max-width: var(--column-width);
+		margin: var(--column-margin-top) auto 0 auto;
+	}
+</style>
---
+++ my-app/src/routes/index.svelte
@@ -0,0 +1,46 @@
+<script context="module">
+	export const prerender = true;
+</script>
+
+<script>
+	import Counter from '$lib/Counter.svelte';
+</script>
+
+<svelte:head>
+	<title>Welcome</title>
+</svelte:head>
+
+<section>
+	<h1>
+		<div class="CalPic">
+			<picture>
+				<source srcset="cal-welcome.webp" type="image/webp" />
+				<img src="cal-welcome.png" alt="Welcome" />
+			</picture>
+		</div>
+
+		to my random default<br />landing page.
+	</h1>
+
+	<h2>
+		For now, I have a random counter that comes with the default page.
+	</h2>
+
+	<Counter />
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+
+	h1 {
+		width: 100%;
+	}
+
+
+</style>
